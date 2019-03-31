0cac47a1c3f0cd98684583947d7001ba72586987
isaac calvo ochoa <isaaccalvoochoa@gmail.com>
2019-03-31 14:15:06 -0700

    Tercer Commit funcionando hasta el video 12. Se creo el Repositorio Generico

---
+++ Shop.Web/Data/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Shop.Web.Data
+{
+    using Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IProductRepository : IGenericRepository<Product> 
+    {
+
+    }
+}
